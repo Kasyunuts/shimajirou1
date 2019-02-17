45bf6af1f56ce0183404aca17d129755cc9cbe1f
黒川雄祐 <kurokawayusuke@kurokawatakeshiyuu-no-MacBook-Air.local>
2019-02-17 17:21:17 +0900

    クリックしたら緯度経度が出る

--- sample1/main.mjs
+++ sample1/main.mjs
@@ -8,8 +8,10 @@ async function main() {
   // 地図を初期化
   const map = new Y.Map('map');
   const zoomLevel = 17;
-  const lng = 35.68227528;
-  const lat = 139.73310240;
+  //const lng = 35.68227528;
+  //const lat = 139.73310240;
+  const lng = 34.71171803;
+  const lat = 135.32595758;
   map.drawMap(new Y.LatLng(lng, lat), zoomLevel, Y.LayerSetId.NORMAL);
 
   // コントロールの追加
--- sample3/main.mjs
+++ sample3/main.mjs
@@ -38,6 +38,25 @@ async function requestGeoCoderAPI(query) {
 }
 
 /**
+ * Yahoo!ジオコーダAPIを使って該当住所の位置情報を検索する
+ * @param {string} float 検索したい住所のクエリ文字列
+ * @return {Feature[]} 位置情報の一覧
+ */
+async function requestGeoPlaceNameAPI(float) {
+  const response = await axios({
+    url: 'https://yusukeshukatsu0930-eval-test.apigee.net/yusukeblack/geocode/V1/geoCoder',
+    params: {
+      float,
+      output: 'json',
+    },
+  });
+  const { data } = response;
+  // APIから受け取った内容をコンソールに表示
+  console.log(JSON.stringify(data, undefined, 2));
+  return data.ResultInfo.Count > 0 ? data.Feature : [];
+}
+
+/**
  * マップの中心を指定した位置情報に移動する
  * @param {YMap} map 表示するマップのインスタンス
  * @param {object} options 位置情報(lat, lng)を含んだパラメータ
@@ -55,6 +74,17 @@ function drawMap(map, options) {
 function parseCoordinates(str) {
   // 入力Coordinatesは"経度,緯度"のフォーマットで来る
   const [lng, lat] = str.trim().split(',');
+  return { lng: Number(lng), lat: Number(lat) };
+}
+
+/**
+ * ジオコーダAPIの検索結果のCoordinatesの文字列をパースして数値に変換する
+ * @param {string} str 緯度経度を表す文字列
+ * @return {object} 緯度(lat)経度(lng)
+ */
+function parseCoordinates2(str) {
+  // クリック取得は"緯度,経度"のフォーマットで来る
+  const [lat, lng] = str.trim().split(',');
   return { lng: Number(lng), lat: Number(lat) };
 }
 
@@ -84,8 +114,16 @@ async function main() {
         resultText.textContent = `場所: ${first.Name}, 緯度: ${lct.lat}, 経度: ${lct.lng}`;
         // 地図を移動してラベル追加
         drawMap(map, lct);
-        const label = new Y.Label(new Y.LatLng(lct.lat, lct.lng), first.Name);
+
+        //緯度経度を地名で表示
+        //const label = new Y.Label(new Y.LatLng(lct.lat, lct.lng), first.Name);
+        //緯度経度をそのまま表示
+        var _llat= "緯度:" + lct.lat + " " + "経度:" + lct.lng
+        const label = new Y.Label(new Y.LatLng(lct.lat, lct.lng), _llat);
         map.addFeature(label);
+        //マーカー
+        var marker = new Y.Marker(new Y.LatLng(lct.lat,lct.lng));
+        map.addFeature(marker);
       } else {
         resultText.textContent = '正しい住所を入力してください';
       }
@@ -93,6 +131,28 @@ async function main() {
     // 戻り値をfalseにして、formのsubmitが常に成功しないようにする
     return false;
   };
+
+  map.bind('click', function(latlng){
+    //const lct = parseCoordinates2(latlng.toString());
+    // 地図を移動してラベル追加
+    //drawMap(map, lct);
+
+    //const geoFeatures = await requestGeoPlaceNameAPI(query);
+    //const resultText = document.getElementById('result-text');
+    //const [first] = geoFeatures;
+
+    //緯度経度を地名で表示
+    //const label = new Y.Label(new Y.LatLng(lct.lat, lct.lng), first.Name);
+    //緯度経度をそのまま表示
+    //var _llat= "緯度:" + lct.lat + " " + "経度:" + lct.lng
+    //const label = new Y.Label(new Y.LatLng(lct.lat, lct.lng), _llat);
+    //map.addFeature(label);
+    //マーカー
+    //var marker = new Y.Marker(new Y.LatLng(lct.lat,lct.lng));
+    //map.addFeature(marker);
+    alert(latlng.toString());
+  });
+
 }
 
 main();
